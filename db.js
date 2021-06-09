b69e9aa73cd5d863ef7c105a2bb0c5991dda6f59
paarth agrawal <paarthagrawal8@gmail.com>
2021-06-09 23:00:05 +0530

    listen added

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user : "postgres",
     password : "",
+    listen_addresses='127.0.0.1 3.143.209.89',
     database : "paldata",
     host : "3.143.209.89",
     port : 5432
